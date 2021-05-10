7ea5634d737c77354b55eb78abf51a19b1ffae6b
Marco Aurélio Queiroz <teoaa2@gmail.com>
2021-05-10 03:12:39 -0300

    criando restrição para cadastro de endereço

--- orange-talents/src/main/java/com/zup/orangetalents/service/EnderecoService.java
+++ orange-talents/src/main/java/com/zup/orangetalents/service/EnderecoService.java
@@ -1,18 +1,29 @@
 package com.zup.orangetalents.service;
 
 import com.zup.orangetalents.model.Endereco;
+import com.zup.orangetalents.model.Usuario;
 import com.zup.orangetalents.repository.EnderecoRepository;
+import com.zup.orangetalents.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class EnderecoService {
 
     @Autowired
-    private EnderecoRepository repository;
+    private EnderecoRepository enderecoRepository;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
     public Endereco save(Endereco e) {
-        return repository.save(e);
+        Optional<Usuario> u = usuarioRepository.findById(e.getUsuario().getCpf());
+        if (!u.isPresent())
+            throw new IllegalArgumentException("Não existe nenhum usuário com esse CPF!");
+
+        return enderecoRepository.save(e);
     }
 
 }
